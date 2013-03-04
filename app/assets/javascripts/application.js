b39246ecfacc4f190c094ac89c7efc24e2b697de
weyewe <w.yunnal@gmail.com>
2013-03-04 13:49:36 +0700

    fix application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
-//= require_tree ./app/model 
-//= require_tree ./app/store 
-//= require_tree ./app/view 
-//= require_tree ./app/controller 
+//= require_tree ./app/model
+//= require_tree ./app/store
+//= require_tree ./app/view
+//= require_tree ./app/controller
 //= require app
